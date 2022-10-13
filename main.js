b62e83373cab28f5ff78b3fa8c911b076470b569
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2022-10-13 22:13:18 +0330

    fixing the transition problem in the begining

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 // adding width and height to the container
 window.addEventListener("load" , PutClockDimension);
+window.addEventListener("load" , ChangeClockTransition);
 window.addEventListener("resize" , PutClockDimension);
+
+var ANALOG__CLOCK = document.getElementById("Analog__Clock");
+var PresentTime = new Date() , PresentSecondClone;
+var PresentHour = PresentTime.getHours();
+var PresentMinute = PresentTime.getMinutes();
+var PresentSecond =  PresentTime.getSeconds();
+const HOUR__ARROW = document.querySelector(".hour__arrow--container");
+const MINUTE__ARROW = document.querySelector(".minute__arrow--container");
+const SECOND__ARROW = document.querySelector(".second__arrow--container");
+
+function ChangeClockTransition(){
+    setTimeout(() => {
+        var ARROW__CONTAINERS = [HOUR__ARROW , MINUTE__ARROW , SECOND__ARROW];
+        for(var i = 0; i < ARROW__CONTAINERS.length; i++){
+            ARROW__CONTAINERS[i].style.transition = "all .05s ease-in-out";
+        }
+    }, 1200);
+}
 
 function PutClockDimension(){
     var WINDOW__WIDTH = window.innerWidth;
-    var ANALOG__CLOCK = document.getElementById("Analog__Clock")
     if(WINDOW__WIDTH > 700){
         ANALOG__CLOCK.style.width = `630px`;
         ANALOG__CLOCK.style.height = `630px`;        
@@ -13,14 +31,6 @@ function PutClockDimension(){
         ANALOG__CLOCK.style.height = `${(WINDOW__WIDTH / 5) * 4}px`;
     };   
 }
-
-var PresentTime = new Date() , PresentSecondClone;
-var PresentHour = PresentTime.getHours();
-var PresentMinute = PresentTime.getMinutes();
-var PresentSecond =  PresentTime.getSeconds();
-const HOUR__ARROW = document.querySelector(".hour__arrow--container");
-const MINUTE__ARROW = document.querySelector(".minute__arrow--container");
-const SECOND__ARROW = document.querySelector(".second__arrow--container");
 
 setInterval(() => {
     PresentSecond += 1;
